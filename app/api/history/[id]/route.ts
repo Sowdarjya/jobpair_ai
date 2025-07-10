96e766e7f5ad24a233440453c562f893e42ae613
Sowdarjya <kolaysowdarjya@gmail.com>
2025-07-10 11:03:58 +0530

    Fixed deployment errors

--- app/api/history/[id]/route.ts
+++ app/api/history/[id]/route.ts
@@ -5,7 +5,7 @@ import { NextRequest, NextResponse } from "next/server";
 export async function GET(
   req: NextRequest,
   { params }: { params: { id: string } }
-) {
+): Promise<NextResponse> {
   try {
     const { userId } = await auth();
 
